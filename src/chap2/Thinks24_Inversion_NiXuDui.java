3947bf95b505891111debc908169af198540de17
zhuxiuwei <jlu_zxw@126.com>
2015-09-17 22:44:44 +0800

    update niXuDui

--- src/chap2/Thinks24_Inversion_NiXuDui.java
+++ src/chap2/Thinks24_Inversion_NiXuDui.java
@@ -7,12 +7,19 @@ package chap2;
  * 150917
  * 
  * 注意点：
- * 	1. 写的不是很顺畅，第一次写的时候还详细着不用真正排序数组，只统计就行。后来发现必须数组一次次逐渐有序了，算法才是对的，也就是说本算法完全是在merge sort算法上面做加法，而不是做改动。
+ * 	1. 写的不是很顺畅，第一次写的时候还寻思着不用真正排序数组，只统计就行。后来发现必须数组一次次逐渐有序了，算法才是对的，也就是说本算法完全是在merge sort算法上面做加法，而不是做改动。
+ *  2. 递归算法里，初始化和收尾时的处理。 
  */
 public class Thinks24_Inversion_NiXuDui {
 	
 	int count = 0;
-	public void calculate(int a[], int begin, int end){
+	/**
+	 *@return: 逆序对的个数
+	 */
+	public int calculate(int a[], int begin, int end){
+		if(begin == 0 && end == a.length - 1){	//注意点2 - 递归开始	
+			count = 0;
+		}
 		int mid = (begin + end)/2;
 		if(begin < mid)
 			 calculate(a, begin, mid);
@@ -20,11 +27,15 @@ public class Thinks24_Inversion_NiXuDui {
 			 calculate(a, mid + 1, end);
 		
 		count(a, begin, mid, end);
+		if(begin == 0 && end == a.length - 1){	//注意点2 - 递归结束
+			System.out.println(count);
+			return count;	//递归结束的return才是有意义的结果
+		}
+		return 0;	//随便return个东西， 不影响最终结果。
 	}
 	
 	/**
 	 * 思路：模仿merge sort中的merge过程，当两个数组merge时，如果先进入result数组的时右边的数组中的某个数字，那么左边数组中的所有剩下的数字都可以和右边数组这个数字组成一个逆序对。
-	 * return: 逆序对的个数
 	 */
 	public void count(int a[], int begin, int mid, int end){
 		if(a.length == 0)
@@ -45,7 +56,7 @@ public class Thinks24_Inversion_NiXuDui {
 		int l = 0,r = 0;
 		for(int i = begin; i <= end; i++){
 			if(right[r] < left[l]){
-				a[i] = right[r++];
+				a[i] = right[r++];	//注意点1 - 这个算法也真的是要排序的。
 				count += left.length - l - 1;	//比merge sort多了这一步。
 			}else{
 				a[i] = left[l++];
@@ -58,32 +69,21 @@ public class Thinks24_Inversion_NiXuDui {
 		 
 		int e[] = {8,7,2,5,9,8,5};	//10
 		t.calculate(e, 0, e.length - 1);
-		System.out.println(t.count);
-		t.count = 0;
 		
 		int a[] = {8,7,2,5,9,8};	//6
 		t.calculate(a, 0, a.length - 1);
-		System.out.println(t.count);
-		t.count = 0;
 		
 		int b[] = {8,7};	//1
 		t.calculate(b, 0, b.length - 1);
-		System.out.println(t.count);
-		t.count = 0;
 		
 		int c[] = {8};	//0
 		t.calculate(c, 0, c.length - 1);
-		System.out.println(t.count);
-		t.count = 0;
 		
 		int d[] = {};	//0
 		t.calculate(d, 0, d.length - 1);
-		System.out.println(t.count);
-		t.count = 0;
-		
+
 		int f[] = {8,7,2,10, 100, 99, 80, 98};	//8
 		t.calculate(f, 0, f.length - 1);
-		System.out.println(t.count);
-		t.count = 0;
+		
 	}
 }
