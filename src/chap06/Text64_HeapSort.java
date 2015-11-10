f2657658bc70df748cb299f6e10f7ac2780c50d4
zhuxiuwei <jlu_zxw@126.com>
2015-11-10 09:03:31 +0800

    add Text64_HeapSort.java

--- src/chap06/Text64_HeapSort.java
+++ src/chap06/Text64_HeapSort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 /**
  * 堆排序算法
  * @author xiuzhu
+ * 151110
  */
 public class Text64_HeapSort {
 
@@ -12,14 +13,30 @@ public class Text64_HeapSort {
 	Text63_build_max_heap buildMaxHeap = new Text63_build_max_heap();	//建堆
 	
 	public void heap_sort(int a[]){
+		int lastHeapEleIndex = a.length - 1;
 		buildMaxHeap.buildMaxHeap(a);
-		
+		//exchange a[0] and the last element in heap
+		int temp = a[0];
+		a[0] = a[lastHeapEleIndex];
+		a[lastHeapEleIndex] = temp;
+		for (int i = 0; i <= a.length - 2; i++) {
+			lastHeapEleIndex --;
+			heapify.maxHeapify_NoRescursive(a, 0, lastHeapEleIndex);
+			//exchange a[0] and the last element in heap
+			temp = a[0];
+			a[0] = a[lastHeapEleIndex];
+			a[lastHeapEleIndex] = temp;
+		}
 		System.out.println("Sort result: " + Arrays.toString(a));
 	}
 	
 	public static void main(String[] args) {
-		int[] a = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
 		Text64_HeapSort sort = new Text64_HeapSort();
+		int[] a = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+		sort.heap_sort(a);
+		a = new int[]{4, 1};
+		sort.heap_sort(a);
+		a = new int[]{4};
 		sort.heap_sort(a);
 	}
 
