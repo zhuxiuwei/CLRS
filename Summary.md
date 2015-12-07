3f3ac49b95bb91f4e5392e9172177786d461d20a
zhuxiuwei <jlu_zxw@126.com>
2015-12-07 09:32:19 +0800

    finish Find2ndSmallest

--- Summary.md
+++ Summary.md
@@ -1,25 +1,25 @@
 需要注意的的题目总结
 =
 #####第2章 算法基础  
-[二分查找递归非递归算法](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap02/prac235_BinarySearch.java) ★★★  
+[二分查找递归非递归算法](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap02_GettingStarted/prac235_BinarySearch.java) ★★★  
 不是很顺畅，退出情况要思考清楚，这个地方花了些时间，  
 
-[统计逆序对个数](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap02/Thinks24_Inversion_NiXuDui.java) ★★★  
+[统计逆序对个数](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap02_GettingStarted/Thinks24_Inversion_NiXuDui.java) ★★★  
 开始算法思路有问题。而且书上不提示的话，估计想不到用归并排序思想。  
 
 #####第4章 分治策略  
-[最大子数组的线性算法](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap04/MaxSubArray_Lineary.java) ★★★★★  
+[最大子数组的线性算法](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap04_DivideAndConquer/MaxSubArray_Lineary.java) ★★★★★  
  花费大量时间。属于那种递归写起来挺直观，但如果写成“面向过程”的写法算法有很复杂，很容易出错。  
  
- [矩阵乘法的原始算法](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap04/MatrixMultipleOriginal.java) ★★★☆  
+ [矩阵乘法的原始算法](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap04_DivideAndConquer/MatrixMultipleOriginal.java) ★★★☆  
   加和公式写错了，导致结果不对，花费1小时时间。写的太慢！  
   【教训】应该在纸上先列下式子，再写代码。代码上来就写导致出错。刚开始矩阵乘法的算法都记错了就开始乱写。  
 当有多重循环时，从业务逻辑中（如矩阵乘法的计算公式），推导出i,j,k变量分别应该在哪个位置的能力需要加强。  
 
-[矩阵乘法的递归算法](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap04/MatrixMultipleRecursive.java) ★★★★  
+[矩阵乘法的递归算法](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap04_DivideAndConquer/MatrixMultipleRecursive.java) ★★★★  
 【难度】 不是很熟练，在纸上写会有问题。难在递归时 -- 也就是分割矩阵时的建模 --- 怎么抽象出矩阵分割，定义函数签名，怎么处理两个矩阵的加法（后来发现实际“不用”做）。建好模后，写起来还比较顺畅。  
 
-[矩阵乘法的Strassen算法](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap04/MatrixMultipleStrassen.java) ★★☆  
+[矩阵乘法的Strassen算法](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap04_DivideAndConquer/MatrixMultipleStrassen.java) ★★☆  
 【难度】 难度和矩阵乘法递归类似。因为有先前经验，这次还算顺利，虽然也花了不少时间（1小时+），主要是因为式子太多，一不小心就会写错~~更多像是耐心活。  
 
 #####第6章 堆排序  
@@ -30,5 +30,12 @@
 花费时间略长。大概2h+。难点在于，如何表达，从n个list里取“下一个”的代码逻辑。逻辑略复杂，手写会有问题。  
 
 #####第8章 线性时间排序  
-[CountNumberInRange](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap08/CountNumberInRange.java) ★★★  
-[8.2-4]查找0..k之间n个数的数组，落在区间[a..b]内的数字的个数。思路不难，但写的时候有三个地儿出错，如果手写会有问题。
+[CountNumberInRange](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap08_SortInLinerTime/CountNumberInRange.java) ★★★  
+[8.2-4]查找0..k之间n个数的数组，落在区间[a..b]内的数字的个数。思路不难，但写的时候有三个地儿出错，如果手写会有问题。  
+
+#####第9章 中位数和顺序统计量  
+[Find2ndSmallest](https://github.com/zhuxiuwei/CLRS/blob/master/src/chap09_MediansAndOrderStatistics/Find2ndSmallest.java) ★★★  
+[9.1-1] 找到n个不同元素的数组中第二小的元素，最坏情况下需要n+⌈lgn⌉-1次比较的算法？  
+花费时间较长，纸上写写不完。开始的时候思路不清晰。  
+	1. 数组越界问题。  
+	2. 算法思路问题。没想到这种两两比较的快速的算法思路，看的网上的答案。  
--- src/chap09_MediansAndOrderStatistics/Find2ndSmallest.java
+++ src/chap09_MediansAndOrderStatistics/Find2ndSmallest.java
@@ -1,49 +1,115 @@
 package chap09_MediansAndOrderStatistics;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
  * 习题9.1-1
- * 9.1-1 找到n个元素中第二小的元素，最坏情况下需要n+⌈lgn⌉-1次比较的算法？  
+ * 9.1-1 找到n个不同元素中第二小的元素，最坏情况下需要n+⌈lgn⌉-1次比较的算法？  
  * @author xiuzhu
  * 思路： We can compare the elements in a tournament fashion - we split them into pairs, compare each pair and then proceed to compare the winners in the same fashion. We need to keep track of each "match" the potential winners have participated in.
 We select a winner in n−1 matches. At this point, we know that the second smallest element is one of the lgn elements that lost to the smallest ­ each of them is smaller than the ones it has been compared to, prior to losing. 
 In another ⌈lgn⌉−1 comparisons we can find the smallest element out of those. This is the answer we are looking for.  
+	
+	注意点：
+	1. 数组越界问题。
+	2. 算法思路问题。没想到这种两两比较的快速的算法思路，看的网上的答案。
  */
 public class Find2ndSmallest {
 
 	private int smallest = 0;
-	private int compareCount = 0;
-	//'losers' when compare with the smallest. should be ⌈lgn⌉  losers.
-	private ArrayList<Integer> compareTrace = new ArrayList<Integer>();
+	private HashMap<Integer, ArrayList<Integer>> compareTrace = new HashMap<Integer, ArrayList<Integer>>();
 	
-	public int callFind(int a[]) throws Exception{
+	/**
+	 * 算法入口。
+	 */
+	public int findSecondSmallest(Integer a[]) throws Exception{
 		if(a == null || a.length <= 1)
 			throw new Exception("Illegal parameter");
-		smallest = findSmallest(a);
 		
+		//find smallest, meanwhile compareTrace is ready. (n - 1) compare.
+		int smallest = find(a);
 		
+		//find smallest in compareTrace. ⌈lgn⌉−1 compare.
+		int secondSmallest = findSmallest(compareTrace.get(smallest));
+		
+		System.out.println(Arrays.toString(a) + " 2nd smallest: " + secondSmallest);
+		
+		return secondSmallest;
 	}
 	
 	/**
-	 * find smallest in array of n elements. (n-1 compare)
+	 * 主要算法逻辑。是递归调用的。
+	 */
+	private int find(Integer a[]){
+		ArrayList<Integer> tempArray = new ArrayList<Integer>();
+		for (int i = 0; i < a.length - 1; i += 2) {	//！！！注意点：i的终止条件。写成i < a.length的话在奇数个元素的数组会数组越界
+			int smaller = 0, bigger = 0;
+			if(a[i] < a[i+1]){	//compare a[i] and a[i+1]
+				smaller = a[i];
+				bigger = a[i+1];
+			}else{
+				smaller = a[i+1];
+				bigger = a[i];
+			}
+			
+			if(a.length == 2){	//champion found, quit recursive.
+				return smaller;
+			}
+			
+			//add to compareTrace
+			if(!compareTrace.containsKey(smaller)){
+				ArrayList<Integer> t = new ArrayList<Integer>();
+				t.add(bigger);
+				compareTrace.put(smaller, t);
+			}else{
+				ArrayList<Integer> t = compareTrace.get(smaller);
+				t.add(bigger);
+				compareTrace.put(smaller, t);
+			}
+			//add smaller to tempArray for next compare
+			tempArray.add(a[i]);
+		}
+		if(a.length % 2 > 0)	//如果数组是奇数个数，最后一个数没有参与本轮比较，直接加入下轮比较
+			tempArray.add(a[a.length - 1]);
+		return find(tempArray.toArray(new Integer[(int)Math.ceil((double)a.length/2)]));
+	}
+	
+	/**
+	 * find smallest in array. (n-1 compare)
 	 * @param a
 	 * @return
 	 */
 	private int findSmallest(int a[]){
 		int temp = a[0];
 		for (int i = 1; i < a.length; i++) {
-			if(a[i] < temp){
+			if(a[i] < temp)
 				temp = a[i];
-				compareCount ++;	//trace compare count
-			}
 		}
 		return temp;
 	}
+	private int findSmallest(ArrayList<Integer> arrayList){
+		int a[] = new int[arrayList.size()];
+		for (int i = 0; i < arrayList.size(); i++) {
+			a[i] = arrayList.get(i);
+		}
+		return findSmallest(a);
+	}
 	
-	public static void main(String[] args) {
-
+	/**
+	 * get value of ⌈lgn⌉
+	 */
+	private int log2N(int n){
+		//Java没有log2N算法，提供了logeN的。下面的算法，利用log2N=logeN/loge2来计算。
+		return (int)Math.ceil((Math.log(n)/Math.log(2)));
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Find2ndSmallest f = new Find2ndSmallest();
+		f.findSecondSmallest(new Integer[]{1, 2, 100, 98, 8, 7, 6 ,9});
+		f.findSecondSmallest(new Integer[]{1, 2, 100, 98, 8, 7, 6 ,9, 10});
+		f.findSecondSmallest(new Integer[]{1, 3, 100, 98, 8, 7, 6 ,9, 2});
 	}
 
 }
