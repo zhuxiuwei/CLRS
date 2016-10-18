e6286f5f5cb338d1db6131706e9d064738f62d53
zhuxiuwei <jlu_zxw@126.com>
2016-10-18 21:38:07 +0800

    update Prac22_1_5_SquareGraph

--- src/chap22_ElementaryGraphAlgo/Prac22_1_5_SquareGraph.java
+++ src/chap22_ElementaryGraphAlgo/Prac22_1_5_SquareGraph.java
@@ -2,8 +2,9 @@ package chap22_ElementaryGraphAlgo;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * 161018
@@ -39,23 +40,49 @@ public class Prac22_1_5_SquareGraph {
 	 * @param g 有向图G的邻接表表示
 	 * @return 平方图G2的邻接表表示
 	 */
-	public Map<Integer, List<Integer>> transform(Map<Integer, List<Integer>> g){
+	public Map<Integer, Set<Integer>> transform(Map<Integer, Set<Integer>> g){
 		if(g == null)
 			return null;
 		
-		Map<Integer, List<Integer>> g2 = new HashMap<Integer, List<Integer>>();
+		Map<Integer, Set<Integer>> g2 = new HashMap<Integer, Set<Integer>>();
 		
 		for (Integer k: g.keySet()) {
-			List<Integer> neighbors = g.get(k);
-			
+			Set<Integer> neighbors = g.get(k);
+			if(neighbors != null){
+				for (int neighbor: neighbors) {
+					Set<Integer> neighborsOfNeighbor = g.get(neighbor);
+					if(neighborsOfNeighbor != null){
+						for (int neighborOfNeighbor: neighborsOfNeighbor) {
+							Set<Integer> newNeighbors = g2.containsKey(k) ? g2.get(k): new HashSet<Integer>();
+							newNeighbors.add(neighborOfNeighbor);
+							g2.put(k, newNeighbors);
+						}
+					}
+				}
+			}
 		}
 		return g2;
 	}
 	
 	public static void main(String[] args) {
 		Prac22_1_5_SquareGraph p = new Prac22_1_5_SquareGraph();
-		System.out.println(Arrays.deepToString(p.transform(new int[][]{{0,1,1},{0,0,1},{0,0,0}})));
-		System.out.println(Arrays.deepToString(p.transform(new int[][]{{0,1,1},{0,0,1},{1,0,0}})));
+		System.out.println(Arrays.deepToString(p.transform(new int[][]{{0,1,1},{0,0,1},{0,0,0}})));	//[[0, 0, 1], [0, 0, 0], [0, 0, 0]]
+		System.out.println(Arrays.deepToString(p.transform(new int[][]{{0,1,1},{0,0,1},{1,0,0}})));	//[[1, 0, 1], [1, 0, 0], [0, 1, 1]]
+		
+		Map<Integer, Set<Integer>> g = new HashMap<Integer, Set<Integer>>();
+		Set<Integer> s1 = new HashSet<Integer>();
+		s1.add(2);
+		s1.add(3);
+		g.put(1, s1);
+		Set<Integer> s2 = new HashSet<Integer>();
+		s2.add(3);
+		g.put(2, s2);
+		System.out.println(p.transform(g));	//{1=[3]}
+		Set<Integer> s3 = new HashSet<Integer>();
+		s3.add(1);
+		g.put(3, s3);
+		System.out.println(p.transform(g));	//{1=[1, 3], 2=[1], 3=[2, 3]}
+
 	}
 
 }
